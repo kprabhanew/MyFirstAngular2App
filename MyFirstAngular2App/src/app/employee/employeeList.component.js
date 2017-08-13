15dc3766272fa9f14231b1d117aff39ab1c777ba
Carl Matthews <carlmatt@iinet.net.au>
2017-08-13 17:23:31 +1000

    Tutorial 20 - Container and Nested Components

--- MyFirstAngular2App/src/app/employee/employeeList.component.js
+++ MyFirstAngular2App/src/app/employee/employeeList.component.js
@@ -16,21 +16,10 @@ var EmployeeListComponent = (function () {
             { code: 'emp101', name: 'Tom', gender: 'Male', annualSalary: 5500, dateOfBirth: '6/25/1988' },
             { code: 'emp102', name: 'Alex', gender: 'Male', annualSalary: 5700.95, dateOfBirth: '9/6/1982' },
             { code: 'emp103', name: 'Mike', gender: 'Male', annualSalary: 5900, dateOfBirth: '12/8/1979' },
-            { code: 'emp104', name: 'Mary', gender: 'Female', annualSalary: 6500.826, dateOfBirth: '10/14/1980' }
-        ];
-    }
-    EmployeeListComponent.prototype.getEmployees = function () {
-        this.employees = [
-            { code: 'emp101', name: 'Tom', gender: 'Male', annualSalary: 5500, dateOfBirth: '6/25/1988' },
-            { code: 'emp102', name: 'Alex', gender: 'Male', annualSalary: 5700.95, dateOfBirth: '9/6/1982' },
-            { code: 'emp103', name: 'Mike', gender: 'Male', annualSalary: 5900, dateOfBirth: '12/8/1979' },
             { code: 'emp104', name: 'Mary', gender: 'Female', annualSalary: 6500.826, dateOfBirth: '10/14/1980' },
             { code: 'emp105', name: 'Nancy', gender: 'Female', annualSalary: 6700.826, dateOfBirth: '12/15/1982' }
         ];
-    };
-    EmployeeListComponent.prototype.trackByEmpCode = function (index, employee) {
-        return employee.code;
-    };
+    }
     return EmployeeListComponent;
 }());
 EmployeeListComponent = __decorate([
