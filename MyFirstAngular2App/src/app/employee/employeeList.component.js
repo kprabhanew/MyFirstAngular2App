48a428ae3faa0b65c78545b10a76358ed56581b6
Carl Matthews <carlmatt@iinet.net.au>
2017-08-16 16:44:36 +1000

    Tutorial 21 - Component Input Properties

--- MyFirstAngular2App/src/app/employee/employeeList.component.js
+++ MyFirstAngular2App/src/app/employee/employeeList.component.js
@@ -17,9 +17,19 @@ var EmployeeListComponent = (function () {
             { code: 'emp102', name: 'Alex', gender: 'Male', annualSalary: 5700.95, dateOfBirth: '9/6/1982' },
             { code: 'emp103', name: 'Mike', gender: 'Male', annualSalary: 5900, dateOfBirth: '12/8/1979' },
             { code: 'emp104', name: 'Mary', gender: 'Female', annualSalary: 6500.826, dateOfBirth: '10/14/1980' },
-            { code: 'emp105', name: 'Nancy', gender: 'Female', annualSalary: 6700.826, dateOfBirth: '12/15/1982' }
+            { code: 'emp105', name: 'Nancy', gender: 'Female', annualSalary: 6700.826, dateOfBirth: '12/15/1982' },
+            { code: 'emp106', name: 'Steve', gender: 'Male', annualSalary: 7700.481, dateOfBirth: '11/18/1979' }
         ];
     }
+    EmployeeListComponent.prototype.getTotalEmployeesCount = function () {
+        return this.employees.length;
+    };
+    EmployeeListComponent.prototype.getTotalMaleEmployeesCount = function () {
+        return this.employees.filter(function (e) { return e.gender === 'Male'; }).length;
+    };
+    EmployeeListComponent.prototype.getTotalFemaleEmployeesCount = function () {
+        return this.employees.filter(function (e) { return e.gender === 'Female'; }).length;
+    };
     return EmployeeListComponent;
 }());
 EmployeeListComponent = __decorate([
